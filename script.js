ecc7f1ff6ab7fa356dde86ec0709482abbaec517
BrakeLJ <lukejbrake@gmail.com>
2020-10-19 13:31:42 -0400

    finished logic, added more styles

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondDegrees = ((seconds / 60) * 360) + 90;
+
+  const minutes = now.getMinutes();
+  const minuteDegrees = ((minutes / 60) * 360) + 90;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + 90; 
+
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-  console.log(seconds);
+  minHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  console.log(hour);
 }
 
 setInterval(setDate, 1000); 
